97b5199f2d30d00dc36fce55ce9ccdd2cd61b61e
xuzeming <xuzeming@dydev.com>
2022-01-10 09:53:14 +0800

    second commit

--- src/main/java/com/example/hellogit/hello/HelloGit.java
+++ src/main/java/com/example/hellogit/hello/HelloGit.java
@@ -3,5 +3,6 @@ package com.example.hellogit.hello;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git!!");
+        System.out.println("hello git222!!");
     }
 }

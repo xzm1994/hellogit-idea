c9f6255d68ff75ca6ebaae36d916050ab8890334
xuzeming <xuzeming@dydev.com>
2022-01-10 09:53:44 +0800

    third commit

--- src/main/java/com/example/hellogit/hello/HelloGit.java
+++ src/main/java/com/example/hellogit/hello/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git!!");
         System.out.println("hello git222!!");
+        System.out.println("hello git33333!!");
     }
 }
